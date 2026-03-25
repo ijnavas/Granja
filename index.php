986263ea7a189ae828532e200788e0f7b28cdf53
ijnavas <nacho@manadi.es>
2026-03-25 15:55:46 +0100

    indice

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 declare(strict_types=1);
 
 // ROOT_PATH apunta a la misma carpeta donde está index.php
